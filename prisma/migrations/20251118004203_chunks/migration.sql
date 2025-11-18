f6b151699805dbe24b331e6e0f84b5af9f836116
cedricnator <Cedric.kirmayr@gmail.com>
2025-11-17 21:45:18 -0300

    feat(database): add DocumentChunk and DocumentChunkType models with relationships

---
+++ prisma/migrations/20251118004203_chunks/migration.sql
@@ -0,0 +1,33 @@
+-- CreateTable
+CREATE TABLE "document_chunk" (
+    "id" TEXT NOT NULL,
+    "index" INTEGER NOT NULL,
+    "content" TEXT NOT NULL,
+    "embedding" vector(1536) NOT NULL,
+    "keywords" TEXT[],
+    "metadata" JSONB NOT NULL,
+    "document_id" TEXT NOT NULL,
+    "document_chunk_type_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "document_chunk_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "document_chunk_type" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "document_chunk_type_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "document_chunk_type_name_key" ON "document_chunk_type"("name");
+
+-- AddForeignKey
+ALTER TABLE "document_chunk" ADD CONSTRAINT "document_chunk_document_id_fkey" FOREIGN KEY ("document_id") REFERENCES "document"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "document_chunk" ADD CONSTRAINT "document_chunk_document_chunk_type_id_fkey" FOREIGN KEY ("document_chunk_type_id") REFERENCES "document_chunk_type"("id") ON DELETE CASCADE ON UPDATE CASCADE;
