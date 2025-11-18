b053f0b5801e99c20ae217a866ec8722abbade1c
cedricnator <Cedric.kirmayr@gmail.com>
2025-11-18 12:46:04 -0300

    refactor(auth): restructure AuthModule and implement UserController for user management

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,18 +3,25 @@ import { SupabaseModule } from '../supabase/supabase.module';
 import { SupabaseAuthRepository } from './infrastructure/supabase/supabase-auth.repository';
 import { USER_REPOSITORY } from './domain/repositories/user.repository';
 import { CreateUserUseCase } from './application/use-cases/create-user.use-case';
-import { AuthController } from './infrastructure/controllers/auth.controller';
+import { UserController } from './infrastructure/controllers/user.controller';
 import { FindUsersUseCase } from './application/use-cases/find-users.use-case';
 import { UpdateUserUseCase } from './application/use-cases/update-user.use-case';
 import { DeleteUserUseCase } from './application/use-cases/delete-user.use-case';
 import { APP_GUARD } from '@nestjs/core';
 import { RolesGuard } from './infrastructure/guards/roles.guard';
 import { SupabaseAuthGuard } from './infrastructure/guards/supabase-auth.guard';
+import { LoginUseCase } from './application/use-cases/login.use-case';
+import { RegisterUseCase } from './application/use-cases/register.use-case';
+import { RefreshTokenUseCase } from './application/use-cases/refresh-token.use-case';
+import { AuthController } from './infrastructure/controllers/auth.controller';
 
 @Module({
   imports: [SupabaseModule],
-  controllers: [AuthController],
+  controllers: [UserController, AuthController],
   providers: [
+    LoginUseCase,
+    RegisterUseCase,
+    RefreshTokenUseCase,
     CreateUserUseCase,
     FindUsersUseCase,
     UpdateUserUseCase,
