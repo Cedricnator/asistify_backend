354593835b3bf7c25e1b4700d3f63001a1bb2f92
Cucharoth <cucharoth@gmail.com>
2025-12-04 02:15:34 -0300

    feat(cors): add patch to cors

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   app.enableCors({
     origin: true, // Allow all origins in development
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: [
       'Content-Type',
       'Authorization',
